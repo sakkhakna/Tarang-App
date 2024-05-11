b7aa8b8cceb0e4feca36ec30ce1dc7e0d4cab864
Sak Khakna <sthissakkhakna@gmail.com>
2024-05-11 10:33:47 +0700

    add menu and fragment

--- app/src/main/java/com/example/tarang_app/MainActivity.java
+++ app/src/main/java/com/example/tarang_app/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.tarang_app;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.tarang_app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.activity_main);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.menuHome) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.menuInbox) {
+                replaceFragment(new InboxFragment());
+            }  else if (itemId == R.id.menuBooking) {
+                replaceFragment(new BookingFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
