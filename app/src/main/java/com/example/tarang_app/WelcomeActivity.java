1b1623bf46e9157802d178b15255378c1189d8b6
Sak Khakna <sthissakkhakna@gmail.com>
2024-05-15 09:21:15 +0700

    fix sign up

--- app/src/main/java/com/example/tarang_app/WelcomeActivity.java
+++ app/src/main/java/com/example/tarang_app/WelcomeActivity.java
@@ -15,9 +15,8 @@ public class WelcomeActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, SignInActivity.class);
-//                Intent intent = new Intent(WelcomeActivity.this, SignUpActivity.class);
-//                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+//                Intent intent = new Intent(WelcomeActivity.this, SignInActivity.class);
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
